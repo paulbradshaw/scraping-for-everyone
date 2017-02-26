38e548fdd0e552803ce9385792acf062705d2ddd
Paul Bradshaw <paulonhismobile@gmail.com>
2017-02-26 14:05:35 +0000

    Create scraper14.py

---
+++ scraper14.py
@@ -0,0 +1,10 @@
+import scraperwiki
+html = scraperwiki.scrape('http://inmo.ie/6022')
+import lxml.html
+root = lxml.html.fromstring(html) # turn our HTML into an lxml object
+tds = root.cssselect('td') # get all the <td tags
+for td in tds:
+    print td.text_content()                # just the text inside the HTML tag
+for td in tds:
+     record = { "td" : td.text_content() } # column name and value
+     scraperwiki.sql.save(["td"], record) # save the records one by one
